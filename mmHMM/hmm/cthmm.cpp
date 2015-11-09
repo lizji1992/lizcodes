9aa7502a892d25ce32abb1d4f775361a4eafcb0a
Liz Ji <lizji@Users-MacBook-Pro.local>
2015-11-09 13:08:56 -0800

    add imputation option and write the imputation output

--- mmHMM/hmm/cthmm.cpp
+++ mmHMM/hmm/cthmm.cpp
@@ -48,6 +48,14 @@ using std::pair;
 using std::setw;
 
 
+template <class T> void
+select_vector_elements(const vector<T> &fullset, vector<T> &subset,
+                       const vector<size_t> &index) {
+  for (size_t i = 0; i < index.size(); ++i) {
+    subset.push_back(fullset[index[i]]);
+  }
+}
+
 static void
 load_cpgs(const string &cpgs_file, vector<SimpleGenomicRegion> &cpgs,
           vector<pair<double, double> > &meth, vector<size_t> &reads,
@@ -87,10 +95,9 @@ load_cpgs(const string &cpgs_file, vector<SimpleGenomicRegion> &cpgs,
   }
 }
 
-
 template <class T, class U> static void
 rm_missingdata(const bool VERBOSE, vector<SimpleGenomicRegion> &cpgs,
-               vector<T> &meth, vector<U> &reads, vector<size_t> mytime) {
+               vector<T> &meth, vector<U> &reads, vector<size_t> &mytime) {
   if (VERBOSE)
     cerr << "[REMOVE ZERO READ CPGS]" << endl;
   size_t j = 0;
@@ -106,9 +113,35 @@ rm_missingdata(const bool VERBOSE, vector<SimpleGenomicRegion> &cpgs,
   meth.erase(meth.begin() + j, meth.end());
   reads.erase(reads.begin() + j, reads.end());
   mytime.erase(mytime.begin() + j, mytime.end());
-
+  
   if (VERBOSE)
     cerr << "CPGS RETAINED: " << cpgs.size() << endl << endl;
+}
+
+
+template <class T, class U> static void
+smooth_methylv(const bool VERBOSE, const vector<U> &reads, vector<T> &meth,
+               const vector<size_t> mytime, vector<size_t> &cov_idx,
+               vector<size_t> &miss_idx) {
+  if (VERBOSE)
+    cerr << "[SMOOTH ZERO READ CPGS]" << endl;
+  
+  size_t data_size = reads.size();
+  size_t cov_r = 0;
+  
+  for (size_t i = 0; i < data_size; ++i) {
+    if (reads[i] > 0) {
+      cov_idx.push_back(i);
+      cov_r = i + 1;
+    } else {
+      miss_idx.push_back(i);
+      meth[i].first = 0.5;
+      meth[i].second = -1;
+    }
+  }
+
+  if (VERBOSE)
+    cerr << "CPGS COVERED: " << cov_idx.size() << endl << endl;
 }
 
 
@@ -116,6 +149,7 @@ static void
 get_domain_scores(const vector<int> &classes,
                   const vector<pair<double, double> > &meth,
                   vector<double> &scores) {
+  //cerr << "GET DOMAIN SCORES" << endl;
   static const int CLASS_ID = 1;
   size_t n_cpgs = 0;
   bool in_domain = false;
@@ -123,7 +157,9 @@ get_domain_scores(const vector<int> &classes,
   for (size_t i = 0; i < classes.size(); ++i) {
     if (classes[i] == CLASS_ID) {
       in_domain = true;
-      score += 1.0 - (meth[i].first/(meth[i].first + meth[i].second));
+      const double denom = (meth[i].second < 0) ?
+                            1 : (meth[i].first + meth[i].second);
+      score += 1.0 - (meth[i].first / denom);
       ++n_cpgs;
     }
     else if (in_domain) {
@@ -137,14 +173,15 @@ get_domain_scores(const vector<int> &classes,
 
 static void
 shuffle_cpgs(TwoVarHMM &hmm, vector<pair<double, double> > meth,
-             const vector<size_t> &mytime, vector<double> &domain_scores) {
+             const vector<size_t> &mytime, vector<double> &domain_scores,
+             bool IMPUT = false) {
   
   srand(time(0) + getpid());
   random_shuffle(meth.begin(), meth.end());
   
   vector<int> classes;
   vector<double> scores;
-  hmm.PosteriorDecoding(meth, mytime, classes, scores);
+  hmm.PosteriorDecoding(meth, mytime, classes, scores, IMPUT);
   get_domain_scores(classes, meth, domain_scores);
   sort(domain_scores.begin(), domain_scores.end());
 }
@@ -154,30 +191,41 @@ static void
 assign_p_values(const vector<double> &random_scores,
                 const vector<double> &observed_scores,
                 vector<double> &p_values) {
-  const double n_randoms =
-  random_scores.size() == 0 ? 1 : random_scores.size();
-  for (size_t i = 0; i < observed_scores.size(); ++i)
-    p_values.push_back((random_scores.end() -
-                        upper_bound(random_scores.begin(),
-                                    random_scores.end(),
-                                    observed_scores[i]))/n_randoms);
+  //cerr << "ASSIGN P VALUES" << endl;
+  const size_t n_random_scores = random_scores.size();
+  const double n_randoms = n_random_scores == 0 ? 1 : n_random_scores;
+  
+  if (n_random_scores == 0)
+    for (size_t i = 0; i < observed_scores.size(); ++i) {
+      p_values.push_back(0);
+    }
+  else
+    for (size_t i = 0; i < observed_scores.size(); ++i) {
+      p_values.push_back((random_scores.end() -
+                          upper_bound(random_scores.begin(),
+                                      random_scores.end(),
+                                      observed_scores[i]))/n_randoms);
+    }
 }
 
 
 double
 get_fdr_cutoff(const vector<double> &scores, const double fdr) {
+
   if (fdr <= 0)
     return numeric_limits<double>::max();
   else if (fdr > 1)
     return numeric_limits<double>::min();
+
   vector<double> local(scores);
   std::sort(local.begin(), local.end());
+
   size_t i = 0;
   for (; i < local.size() - 1 &&
        local[i+1] < fdr*static_cast<double>(i+1)/local.size(); ++i);
+
   return local[i];
 }
-
 
 
 
@@ -187,6 +235,8 @@ build_domains(const bool VERBOSE,
               const vector<double> &post_scores,
               const vector<int> &classes,
               vector<GenomicRegion> &domains) {
+  //cerr << "ENTER BUILD DOMAINS  ";
+  //cerr << std::accumulate(classes.begin(), classes.end(), 0) << endl;
   static const int CLASS_ID = 1;
   size_t n_cpgs = 0, n_domains = 0, prev_end = 0;
   bool in_domain = false;
@@ -213,7 +263,6 @@ build_domains(const bool VERBOSE,
 }
 
 
-
 int
 main(int argc, const char **argv) {
 
@@ -222,6 +271,8 @@ main(int argc, const char **argv) {
     size_t max_iterations = 10;
     
     // run mode flags
+    bool IMPUT = false;
+    bool TRAINIMPUT = false;
     bool VERBOSE = false;
     bool NOFDR = false;
     
@@ -229,8 +280,13 @@ main(int argc, const char **argv) {
     double tolerance = 1e-10;
     double min_prob  = 1e-10;
 
+    // transition distribution on time
+    bool NO_RATE_EST = false;
+    double fg_rate = 0.002;
+    double bg_rate = 0.02;
+    
     string params_in_file, params_out_file;
-    string outfile, scores_file;
+    string outfile, scores_file, imput_file;
     
     /****************** COMMAND LINE OPTIONS ********************/
     OptionParser opt_parse(strip_path(argv[0]), "Program for identifying "
@@ -239,13 +295,23 @@ main(int argc, const char **argv) {
                       false, outfile);
     opt_parse.add_opt("scores", 's', "scores file (WIG format)",
                       false, scores_file);
+    opt_parse.add_opt("fulldata", 'd', "imputated full data file (BED format)",
+                      false, imput_file);
     opt_parse.add_opt("itr", 'i', "max iterations", false, max_iterations);
+    opt_parse.add_opt("imput", 'I', "imputation", false, IMPUT);
+    opt_parse.add_opt("train_imput", 'T', "imputation training",
+                      false, TRAINIMPUT);
     opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
     opt_parse.add_opt("no_fdr_control", 'f', "fdr_control", false, NOFDR);
-    opt_parse.add_opt("params-in", 'P', "HMM parameters file (no training)", 
-                      false, params_in_file);
-    opt_parse.add_opt("params-out", 'p', "write HMM parameters to this file", 
-                      false, params_out_file);
+    //opt_parse.add_opt("params-in", 'P', "HMM parameters file (no training)",
+    //                  false, params_in_file);
+    //opt_parse.add_opt("params-out", 'p', "write HMM parameters to this file",
+    //                  false, params_out_file);
+    opt_parse.add_opt("fgrate", 'F', "fg rate", false, fg_rate);
+    opt_parse.add_opt("bgrate", 'B', "bg rate", false, bg_rate);
+    opt_parse.add_opt("no_rate_est", 'r', "no rate estimation",
+                      false, NO_RATE_EST);
+    
     
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
@@ -282,9 +348,15 @@ main(int argc, const char **argv) {
       << "MEAN COVERAGE: "
       << accumulate(reads.begin(), reads.end(), 0.0)/reads.size()
       << endl << endl;
-
     
-    rm_missingdata(VERBOSE, cpgs, meth, reads, time);
+    vector<size_t> cov_idx;
+    vector<size_t> miss_idx;
+    if (!IMPUT) {
+      rm_missingdata(VERBOSE, cpgs, meth, reads, time);
+    }
+    else {
+      smooth_methylv(VERBOSE, reads, meth, time, cov_idx, miss_idx);
+    }
     
     // set-up distributions
     double fg_alpha = 0;
@@ -292,8 +364,14 @@ main(int argc, const char **argv) {
     double bg_alpha = 0;
     double bg_beta = 0;
 
-    const double n_reads =
-      accumulate(reads.begin(), reads.end(), 0.0)/reads.size();
+    
+    double n_reads;
+    if (!IMPUT) {
+      n_reads = accumulate(reads.begin(), reads.end(), 0.0)/reads.size();
+    } else {
+      n_reads = accumulate(reads.begin(), reads.end(), 0.0)/cov_idx.size();
+    }
+    
     fg_alpha = 0.33*n_reads;
     fg_beta = 0.67*n_reads;
     bg_alpha = 0.67*n_reads;
@@ -308,17 +386,28 @@ main(int argc, const char **argv) {
     double p_ft = 1e-10;
     double p_bt = 1e-10;
 
-    // transition distribution on time
-    double fg_rate = 0.02;
-    double bg_rate = 0.002;
    
     // HMM initialization
-    TwoVarHMM hmm(tolerance, min_prob, max_iterations, VERBOSE);
+    TwoVarHMM hmm(tolerance, min_prob, max_iterations, VERBOSE, NO_RATE_EST);
+
+    if (TRAINIMPUT) hmm.set_trainimput(TRAINIMPUT, cov_idx);
+    
+    // HMM set parameters
     hmm.set_parameters(fg_emission, bg_emission, fg_rate, bg_rate,
                        p_sf, p_sb, p_ft, p_bt);
     
+    vector< pair<double, double> > cmeth;
+    vector<size_t> ctime;
+    
     // HMM training
-    double score = hmm.BaumWelchTraining(meth, time);
+    if (IMPUT && !TRAINIMPUT) {
+        select_vector_elements(meth, cmeth, cov_idx);
+        select_vector_elements(time, ctime, cov_idx);
+        hmm.BaumWelchTraining(cmeth, ctime);
+    } else {
+      hmm.BaumWelchTraining(meth, time);
+    }
+    
     
 /*
     
@@ -332,29 +421,33 @@ main(int argc, const char **argv) {
      * STEP 5: DECODE THE DOMAINS
      */
     
-    
     vector<int> classes;
     vector<double> scores;
-    hmm.PosteriorDecoding(meth, time, classes, scores);
-    
-    
+    if (IMPUT) {
+      hmm.PosteriorDecoding(meth, time, classes, scores, IMPUT);
+    } else {
+      hmm.PosteriorDecoding(meth, time, classes, scores);
+    }
+   
+
     vector<double> domain_scores;
     get_domain_scores(classes, meth, domain_scores);
     
     vector<double> random_scores;
-    shuffle_cpgs(hmm, meth, time, random_scores);
+    shuffle_cpgs(hmm, meth, time, random_scores, IMPUT);
     
     vector<double> p_values;
     assign_p_values(random_scores, domain_scores, p_values);
     
-    
-    double fdr_cutoff = std::numeric_limits<double>::max();
+    //double fdr_cutoff = std::numeric_limits<double>::max();
+    double fdr_cutoff = numeric_limits<double>::max();
     if (fdr_cutoff == numeric_limits<double>::max())
       fdr_cutoff = get_fdr_cutoff(p_values, 0.01);
 
   
     vector<GenomicRegion> domains;
     build_domains(VERBOSE, cpgs, scores, classes, domains);
+    
     
     std::ofstream of;
     if (!outfile.empty()) of.open(outfile.c_str());
@@ -378,6 +471,17 @@ main(int argc, const char **argv) {
       }
     }
 
+    if (IMPUT) {
+      if (!imput_file.empty()) {
+        std::ostream *out_imput = new std::ofstream(imput_file.c_str());
+        for (size_t i = 0; i < cpgs.size(); ++i) {
+          const double denom = (meth[i].second < 0) ?
+                                1 : (meth[i].first + meth[i].second);
+          *out_imput << cpgs[i] << '\t' << meth[i].first / denom << '\t'
+                     << reads[i] << endl;
+        }
+      }
+    }
   }
   catch (SMITHLABException &e) {
     cerr << "ERROR:\t" << e.what() << endl;
