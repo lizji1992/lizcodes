6f103ae5677826386a65b2f23ce786ccf9d6f926
Liz Ji <lizji@Users-MacBook-Pro.local>
2015-12-03 15:27:02 -0800

    simulate hmrs and methylation levels using ct markov chain

--- mmHMM/hmm/CTHMM_sim.cpp
+++ mmHMM/hmm/CTHMM_sim.cpp
@@ -1,8 +1,25 @@
-/* HMR simulator
- * Song Qiang <qiang.song@usc.edu> 2015-2016
+/*
+ * HMR simulator using continuous-time markov
  *
- * No coordinates of CpGs are taken into consideration. The simualted CpGs are 
- * put onto "1,2,3,4,5 ..." of "chromosome Z" in the output.
+ * Copyright (C) 2015-2016 University of Southern California
+ *                         Andrew D Smith
+ *
+ * Author: Andrew D. Smith, Xiaojing Ji
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 #include <ctime>
